828bcca553fc16ea96ceab8f16e17920a59d286f
Emman-n <enuneza937@gmail.com>
2024-02-07 11:18:28 -0600

    committ7

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 
 import cors from "cors";
-import config from './config.js';
 import mysql from 'mysql2';
 
 
@@ -40,101 +39,97 @@ db.connect((err) => {
 });
 
 
+// app.get("/", (req, res) => {
+//   const sql= "SELECT * FROM hira";
 
-app.get("/", (req, res) => {
-  const sql= "SELECT * FROM hira";
-
-  db.query(sql,(err,result)=>{
-    if (err) return res.json({Message:"fuuuuck"})
-    return res.json(result);
-  })
- });
+//   db.query(sql,(err,result)=>{
+//     if (err) return res.json({Message:"fuuuuck"})
+//     return res.json(result);
+//   })
+//  });
 
  
+// app.get("/", (req, res) => {
+//   res.json("hello this is the backend :O");
+// });
 
-app.get("/", (req, res) => {
-  res.json("hello this is the backend :O");
-});
 
+// //--------------------------------------
+// function findResponseHira(req, res) {
+//   const hiraId = req.params.id; // Extract the parameter from the request
 
+//   const sql = `SELECT * FROM hiradata.hira WHERE id = 1`;
+//   db.query(sql, [hiraId], (err, data) => {
+//     if (err) {
+//       return res.json(err);
+//     }else{
+//       return res.status(200).json(data);
+//     }
+ 
+//   });
+// }
+// //---------------------------------------
+// app.get("/hira/:id", (req, res) => {
+//   findResponseHira(req, res);
+// });
 
 
 //--------------------------------------
-function findResponseHira(req, res) {
-  const hiraId = req.params.id; // Extract the parameter from the request
+// function findResponseKata(req, res) {
+//   const hiraId = req.params.id; // Extract the parameter from the request
 
-  const sql = `SELECT * FROM hiradata.hira WHERE id = 1`;
-  db.query(sql, [hiraId], (err, data) => {
-    if (err) {
-      return res.json(err);
-    }else{
-      return res.status(200).json(data);
-    }
+//   const sql = `SELECT * FROM kata WHERE id = ?`;
+//   db.query(sql, [hiraId], (err, data) => {
+//     if (err) {
+//       return res.json(err);
+//     }else{
+//       return res.status(200).json(data);
+//     }
  
-  });
-}
-//---------------------------------------
-app.get("/hira/:id", (req, res) => {
-  findResponseHira(req, res);
-});
-
-
-//--------------------------------------
-function findResponseKata(req, res) {
-  const hiraId = req.params.id; // Extract the parameter from the request
-
-  const sql = `SELECT * FROM kata WHERE id = ?`;
-  db.query(sql, [hiraId], (err, data) => {
-    if (err) {
-      return res.json(err);
-    }else{
-      return res.status(200).json(data);
-    }
- 
-  });
-}
-//---------------------------------------
-app.get("/kata/:id", (req, res) => {
-  findResponseKata(req, res);
-});
+//   });
+// }
+// //---------------------------------------
+// app.get("/kata/:id", (req, res) => {
+//   findResponseKata(req, res);
+// });
 
 
 
 
-//---------------------------------------
-app.get("/hira", (req, res) => {
-  const q = `SELECT * FROM hira `;
-  db.query(q, (err, data) => {
-    if (err) return res.json(err);
-    return res.json(data);
-  });
-});
-//---------------------------------------
-app.get("/kata", (req, res) => {
-  const q = `SELECT * FROM kata `;
-  db.query(q, (err, data) => {
-    if (err) return res.json(err);
-    return res.json(data);
-  });
-});
+// //---------------------------------------
+// app.get("/hira", (req, res) => {
+//   const q = `SELECT * FROM hira `;
+//   db.query(q, (err, data) => {
+//     if (err) return res.json(err);
+//     return res.json(data);
+//   });
+// });
+// //---------------------------------------
+// app.get("/kata", (req, res) => {
+//   const q = `SELECT * FROM kata `;
+//   db.query(q, (err, data) => {
+//     if (err) return res.json(err);
+//     return res.json(data);
+//   });
+// });
 
-//---------------------------------
-app.get("results/:id", (req, res) => {
-  const sql = `SELECT * FROM hira where id= ? `;
-  const id = req.params.id;
-  db.query(sql,[id], (err, data) => {
-    if (err) return res.json(err);
-    return res.json(data);
-  });
-});
+// //---------------------------------
+// app.get("results/:id", (req, res) => {
+//   const sql = `SELECT * FROM hira where id= ? `;
+//   const id = req.params.id;
+//   db.query(sql,[id], (err, data) => {
+//     if (err) return res.json(err);
+//     return res.json(data);
+//   });
+// });
 
-app.get("/hira/:id", (req, res) => {
-  const q = `SELECT * FROM hira where = ${req}`;
-  db.query(q, (err, data) => {
-    if (err) return res.json(err);
-    return res.json(data);
-  });
-});
+// app.get("/hira/:id", (req, res) => {
+//   const q = `SELECT * FROM hira where = ${req}`;
+//   db.query(q, (err, data) => {
+//     if (err) return res.json(err);
+//     return res.json(data);
+//   });
+// });
 
 
  
