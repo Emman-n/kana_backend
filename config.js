9f51b0c0899a6fe16ef0f37bc8d71448d3dc13d3
Emman-n <enuneza937@gmail.com>
2024-02-12 19:14:55 -0600

    committ17

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
-// config.js
-import 'dotenv/config';
+// // config.js
+// import 'dotenv/config';
 
 
-const config = {
-    database: {
-        host: process.env.DB_HOST || 'default_host',
-        username: process.env.DB_USERNAME || 'default_username',
-        password: process.env.DB_PASSWORD || 'default_password',
-        databaseName: process.env.DB_NAME || 'default_database',
-      },
-      apiKey: process.env.API_KEY || 'default_api_key',
-      port: process.env.PORT  ,  // default to 3000 if PORT is not specified
-      // Add other configuration variables as needed
-    };
+// const config = {
+//     database: {
+//         host: process.env.DB_HOST || 'default_host',
+//         username: process.env.DB_USERNAME || 'default_username',
+//         password: process.env.DB_PASSWORD || 'default_password',
+//         databaseName: process.env.DB_NAME || 'default_database',
+//       },
+//       apiKey: process.env.API_KEY || 'default_api_key',
+//       port: process.env.PORT  ,  // default to 3000 if PORT is not specified
+//       // Add other configuration variables as needed
+//     };
   
-  export default config;
+//   export default config;
